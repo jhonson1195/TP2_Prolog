285723e78bf4181df57e24928c9d32c5455e729c
albsolano <albersolano@gmail.com>
2015-10-17 15:03:01 -0600

    Update bd.pl

--- bd.pl
+++ bd.pl
@@ -54,17 +54,17 @@ riesgo('El Salvador',_,'Alto').
 riesgo('Honduras',_,'Alto').
 riesgo('Guatemala',_,'Alto').
 riesgo('Mexico',_,'Alto').
-riesgo('Estados Unidos','Alaska','Alto').
-riesgo('Canada','Columbia Britanica','Alto').
-riesgo('Marruecos',_,'Alto').
+riesgo('Alaska',_,'Alto').
+riesgo('Canada','British Columbia','Alto').
+riesgo('Morocco',_,'Alto').
 riesgo('Portugal',_,'Alto').
-riesgo('Grecia',_,'Alto').
+riesgo('Greece',_,'Alto').
 riesgo('Turkia',_,'Alto').
 riesgo('Pakistan','Sindh','Alto').
 riesgo('Pakistan','Punyab','Alto').
 riesgo('India',_,'Alto').
 riesgo('Bangladesh',_,'Alto').
-riesgo('Tailandia',_,'Alto').
+riesgo('Thailand',_,'Alto').
 riesgo('Indonesia',_,'Alto').
 riesgo('China','Pekin','Alto').
 riesgo('China','Shanghai','Alto').
@@ -74,91 +74,105 @@ riesgo('China','Fujian','Alto').
 riesgo('China','Jiangxi','Alto').
 riesgo('Taiwan',_,'Alto').
 riesgo('Hong Kong',_,'Alto').
-riesgo('Filipinas',_,'Alto').
-riesgo('Birmania',_,'Alto').
-riesgo('Rusia','Krai de Primorie','Alto').
-riesgo('Rusia','Krai de Jabarovsk','Alto').
-riesgo('Rusia','Oblast de Magadan','Alto').
-riesgo('Rusia','Krai de Kamchatka','Alto').
-riesgo('Rusia','Chukotka','Alto').
-riesgo('Korea Del Sur',_,'Alto').
-riesgo('Japon',_,'Alto').
-riesgo('Nueva Guinea',_,'Alto').
+riesgo('Philippines',_,'Alto').
+riesgo('Burma',_,'Alto').
+riesgo('Russia','Primorsky Krai','Alto').
+riesgo('Russia','Khabarovsk Krai','Alto').
+riesgo('Russia','Magadan Oblast','Alto').
+riesgo('Russia','Kamchatka Krai','Alto').
+riesgo('Russia','Chukotka','Alto').
+riesgo('South Corea',_,'Alto').
+riesgo('Japan',_,'Alto').
+riesgo('Papua New Guinea',_,'Alto').
+riesgo('Solomon Islands',_, 'Alto').
+riesgo('Vanuatu', _, 'Alto').
+riesgo('Micronesia', _, 'Alto').
+
+
+
 
 /*Paises con zona de riesgo anaranjada*/
-riesgo('Estados Unidos','California','Medio').
-riesgo('Estados Unidos','Oregon','Medio').
-riesgo('Estados Unidos','Washington','Medio').
+riesgo('United States','California','Medio').
+riesgo('United States','Oregon','Medio').
+riesgo('United States','Washington','Medio').
 riesgo('Venezuela',_,'Medio').
-riesgo('Belice',_,'Medio').
-riesgo('España',_,'Medio').
+riesgo('Belize',_,'Medio').
+riesgo('Spain',_,'Medio').
 riesgo('Cuba',_,'Medio').
 riesgo('Haiti',_,'Medio').
-riesgo('Republica Dominicana',_,'Medio').
+riesgo('Dominican Republic',_,'Medio').
 riesgo('Puerto Rico',_,'Medio').
-riesgo('Francia',_,'Medio').
-riesgo('Italia',_,'Medio').
+riesgo('France',_,'Medio').
+riesgo('Italy',_,'Medio').
 riesgo('Algeria',_,'Medio').
-riesgo('Libia',_,'Medio').
-riesgo('Egipto',_,'Medio').
-riesgo('Tunez',_,'Medio').
+riesgo('Libya',_,'Medio').
+riesgo('Egypt',_,'Medio').
+riesgo('Tunisia',_,'Medio').
 riesgo('Madagascar',_,'Medio').
 riesgo('Turquia',_,'Medio').
 riesgo('Iran',_,'Medio').
-riesgo('Korea Del Norte',_,'Medio').
+riesgo('North Korea',_,'Medio').
 riesgo('Vietnam',_,'Medio').
 riesgo('Singapur',_,'Medio').
-riesgo('Malasia',_,'Medio').
+riesgo('Malaysia',_,'Medio').
 riesgo('Australia',_,'Medio').
-riesgo('Nueva Zelanda',_,'Medio').
+riesgo('New Zealand',_,'Medio').
 riesgo('Oman',_,'Medio').
-riesgo('Emiratos Arabes',_,'Medio').
+riesgo('Arab Emirates',_,'Medio').
+riesgo('New Caledonia', _, 'Medio').
 
 /*Paises con zona de riesgo amarillo*/
-riesgo('Canada','Terranova y Labrador','Bajo').
-riesgo('Canada','Nuevo Brunswick','Bajo').
-riesgo('Canada','Nueva Escocia','Bajo').
+riesgo('Barbados',_ ,'Bajo').
+riesgo('Tonga', _,'Bajo').
+riesgo('Canada','Newfoundland and Labrador','Bajo').
+riesgo('Canada','New Brunswick','Bajo').
+riesgo('Canada','Nova Scotia','Bajo').
 riesgo('Canada','Quebec','Bajo').
-riesgo('Estados Unidos','Wisconsin','Bajo').
-riesgo('Estados Unidos','Ilinois','Bajo').
-riesgo('Estados Unidos','Kentucky','Bajo').
-riesgo('Estados Unidos','Tennessee','Bajo').
-riesgo('Estados Unidos','Misisipi','Bajo').
-riesgo('Estados Unidos','Alabama','Bajo').
-riesgo('Estados Unidos','Indiana','Bajo').
-riesgo('Estados Unidos','Michigan','Bajo').
-riesgo('Estados Unidos','Ohio','Bajo').
-riesgo('Estados Unidos','Georgia','Bajo').
-riesgo('Estados Unidos','Florida','Bajo').
-riesgo('Estados Unidos','Carolina Del Sur','Bajo').
-riesgo('Estados Unidos','Carolina del Norte','Bajo').
-riesgo('Estados Unidos','Virginia','Bajo').
-riesgo('Estados Unidos','Pensilvania','Bajo').
-riesgo('Estados Unidos','Nueva York','Bajo').
-riesgo('Estados Unidos','Massachusetts','Bajo').
-riesgo('Estados Unidos','Nuevo Hampshire','Bajo').
-riesgo('Estados Unidos','Maine','Bajo').
-riesgo('Estados Unidos','Rhode Island','Bajo').
-riesgo('Estados Unidos','Connecticut','Bajo').
-riesgo('Estados Unidos','Nueva Jersey','Bajo').
-riesgo('Estados Unidos','Delaware','Bajo').
-riesgo('Estados Unidos','Maryland','Bajo').
-riesgo('Estados Unidos','Washington DC','Bajo').
-riesgo('Estados Unidos','Virginia Occidental','Bajo').
+riesgo('United States','Wisconsin','Bajo').
+riesgo('United States','Ilinois','Bajo').
+riesgo('United States','Kentucky','Bajo').
+riesgo('United States','Tennessee','Bajo').
+riesgo('United States','Misisipi','Bajo').
+riesgo('United States','Alabama','Bajo').
+riesgo('United States','Indiana','Bajo').
+riesgo('United States','Michigan','Bajo').
+riesgo('United States','Ohio','Bajo').
+riesgo('United States','Georgia','Bajo').
+riesgo('United States','Florida','Bajo').
+riesgo('United States','South Carolina','Bajo').
+riesgo('United States','North Carolina','Bajo').
+riesgo('United States','Virginia','Bajo').
+riesgo('United States','Pensilvania','Bajo').
+riesgo('United States','New York','Bajo').
+riesgo('United States','Massachusetts','Bajo').
+riesgo('United States','New Hampshire','Bajo').
+riesgo('United States','Maine','Bajo').
+riesgo('United States','Rhode Island','Bajo').
+riesgo('United States','Connecticut','Bajo').
+riesgo('United States','New Jersey','Bajo').
+riesgo('United States','Delaware','Bajo').
+riesgo('United States','Maryland','Bajo').
+riesgo('United States','Washington DC','Bajo').
+riesgo('United States','West Virginia','Bajo').
 riesgo('Surinam',_,'Bajo').
 riesgo('Guyana',_,'Bajo').
-riesgo('Guyana Francesa',_,'Bajo').
-riesgo('Irlanda',_,'Bajo').
-riesgo('Inglaterra',_,'Bajo').
-riesgo('Croacia',_,'Bajo').
+riesgo('French Guyana',_,'Bajo').
+riesgo('Ireland',_,'Bajo').
+riesgo('England',_,'Bajo').
+riesgo('Croatia',_,'Bajo').
 riesgo('Albania',_,'Bajo').
 riesgo('Montenegro',_,'Bajo').
-riesgo('Sudafrica',_,'Bajo').
+riesgo('South Africa',_,'Bajo').
 riesgo('Mozambique',_,'Bajo').
 riesgo('Yemen',_,'Bajo').
-riesgo('Gales',_,'Bajo').
-riesgo('Escocia',_,'Bajo').
-riesgo('Camboya',_,'Bajo').
+riesgo('Wales',_,'Bajo').
+riesgo('Scotland',_,'Bajo').
+riesgo('Cambodia',_,'Bajo').
+riesgo('Fiji',_,'Bajo').
+riesgo('Fiji Islands',_,'Bajo').
+
+
+
 
 /*Paises con zona de riesgo muy baja o color gris*/
 riesgo(X,Y,'Muy Bajo'):- not(riesgo(X,Y,'Alto')),not(riesgo(X,Y,'Medio')), not(riesgo(X,Y,'Bajo')).
